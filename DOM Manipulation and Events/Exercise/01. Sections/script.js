22af378cebc8c2c5c197562d1f80e4a27af48dcf
Nikola Madzharov <nmadzharov1912@gmail.com>
2023-01-11 20:27:00 +0200

    DOM Manipulation and Events Exercise  Sections

---
+++ DOM Manipulation and Events/Exercise/01. Sections/script.js
@@ -0,0 +1,19 @@
+function create(words) {
+ let content = document.getElementById(`content`);
+
+ for (const word of words) {
+   
+   let div = document.createElement('div');
+   let paragraph = document.createElement('p');
+   paragraph.textContent = word;
+   paragraph.style.display = 'none';
+   div.appendChild(paragraph);
+   
+   content.appendChild(div);
+
+   div.addEventListener('click', (e) =>{
+    e.currentTarget.children[0].style.display ='block';
+   })
+
+ }
+}
